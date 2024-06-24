314de0d44e96f75256b4da8cc222ddfe11dd95e2
BlueskyUS <catherine_liu99@yahoo.com>
2024-06-23 20:39:22 -0400

    refine the accurate lemmatization

--- quali_text re.py
+++ quali_text re.py
@@ -4,6 +4,7 @@ from collections import Counter
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
@@ -11,6 +12,7 @@ from wordcloud import WordCloud
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
 
 def main():
     # File path to be analyzed
@@ -22,13 +24,16 @@ def main():
     # Tokenize text into words
     words = tokenize_text(text)
     
+    # Lemmatize words
+    lemmatized_words = lemmatize_words(words)
+    
     # Get stop words from nltk and add custom stop words
     stop_words = set(stopwords.words('english'))
     custom_stop_words = {'osu', 'next', '1', 'fit'}
     stop_words.update(custom_stop_words)
     
     # Filter out the stop words
-    filtered_words = [word for word in words if word not in stop_words]
+    filtered_words = [word for word in lemmatized_words if word not in stop_words]
 
     # Count word frequencies using Counter
     frequency_dict = Counter(filtered_words)
@@ -39,7 +44,7 @@ def main():
     # Print the top 10 words with their frequencies
     for word, count in top_10_words:
         print_histogram_bar(word, count)
-
+    
     # Visualize the word cloud
     visualize_wordcloud(frequency_dict)
 
@@ -57,10 +62,19 @@ def tokenize_text(text):
     words = re.findall(r'\w+', text.lower())
     return words
 
+def get_wordnet_pos(word):
+    """Map POS tag to first character lemmatize() accepts"""
+    tag = nltk.pos_tag([word])[0][1][0].upper()
+    tag_dict = {"J": wordnet.ADJ,
+                "N": wordnet.NOUN,
+                "V": wordnet.VERB,
+                "R": wordnet.ADV}
+    return tag_dict.get(tag, wordnet.NOUN)
+
 def lemmatize_words(words):
-    """Lemmatizes a list of words."""
+    """Lemmatizes a list of words with POS tagging."""
     lemmatizer = WordNetLemmatizer()
-    lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+    lemmatized_words = [lemmatizer.lemmatize(word, get_wordnet_pos(word)) for word in words]
     return lemmatized_words
 
 def visualize_wordcloud(word_counts):
